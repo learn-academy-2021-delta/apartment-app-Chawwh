0997c9c28521e45a0d492223520f5124458f008b
Chawwh <chachablueftw@gmail.com>
2021-11-05 16:28:32 -0600

    added mock data and displaying on index

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home"
 import AboutMe from "./pages/AboutMe"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import ApptIndex from './pages/ApptIndex'
+import apartments from "./mockApartments"
 import {
   BrowserRouter as  Router,
   Route,
@@ -12,14 +14,22 @@ import {
 } from "react-router-dom"
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      apartments: apartments
+    }
+  }
   
 render(){
+  console.log(this.state.apartments);
     return (
       <Router>
         <Header {...this.props} />
       <Routes>
         <Route exact path="/" element={ <Home /> } />
         <Route path="/about" element={ <AboutMe /> } />
+          <Route path="/listings"  element={<ApptIndex appts={this.state.apartments} /> } />
       </Routes>
       <Footer />
     </Router>
---
+++ app/javascript/components/mockApartments.js
@@ -0,0 +1,56 @@
+let apartments = [
+    {
+        id:1,
+        street:"42 Wallaby Way",
+        city: "Sydney",
+        state:"New South Wales",
+        manager:"",
+        email:"marlin@findingnemo.com",
+        price:"",
+        bedrooms: 2,
+        bathrooms: 1.5,
+        pets:"",
+        user_id: 1
+    },
+    {
+        id:2,
+        street:"",
+        city:"",
+        state:"",
+        manager:"",
+        email:"",
+        price:"",
+        bedrooms: 3,
+        bathrooms: 2.0,
+        pets:"",
+        user_id: 1
+    },
+    {
+        id:3,
+        street:"123 Sesame Street",
+        city: "",
+        state:"",
+        manager:"",
+        email:"elmo@sesamestreet.org",
+        price:"",
+        bedrooms: 4,
+        bathrooms: 2.5,
+        pets:"",
+        user_id: 1
+    },
+    {
+        id:4,
+        street:"342 Gravelpit Terrace",
+        city: "",
+        state:"",
+        manager:"",
+        email:"fred@flinstones.com",
+        price:"",
+        bedrooms: 1,
+        bathrooms: 1.0,
+        pets:"",
+        user_id: 1
+    }
+]
+
+export default apartments
